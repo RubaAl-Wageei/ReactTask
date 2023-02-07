67b8092bd6f5462b3f1cc7b98dfe0a0d7a2babed
unknown <ruba.salahuddin@gmail.com>
2023-02-07 19:40:02 +0300

    React Task

---
+++ src/Component/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <Navbar bg="dark" variant="dark">
+                    <Container>
+                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#features">Features</Nav.Link>
+                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        </Nav>
+                    </Container>
+                </Navbar>
+            </div>
+        )
+    }
+}
+
+export default Header;
